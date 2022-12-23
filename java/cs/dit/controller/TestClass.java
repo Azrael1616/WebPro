3b2c32721e7d076fd851ccec23b31676ee3e2ddd
inje <58834039+Azrael1616@users.noreply.github.com>
2022-12-23 15:32:31 +0900

    Add files via upload

---
+++ java/cs/dit/controller/TestClass.java
@@ -0,0 +1,11 @@
+package cs.dit.controller;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class TestClass {
+	private String id;
+	private String name;
+}
---
+++ java/cs/dit/sample/Chef.java
@@ -0,0 +1,8 @@
+package cs.dit.sample;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Chef {
+
+}
